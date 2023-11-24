0639037c0c34602ac5222b1475a563a6db6b280b
Vsanotskyi <vsanotskyi@gmail.com>
2023-11-24 19:09:28 +0200

    add span for friends list 'isOnline'

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,18 +1,17 @@
-import css from "./FriendListItem.module.css";
+import css from './FriendListItem.module.css';
 
-export default function FriendListItem({friend}) {
-    const {avatar, name, isOnline} = friend;
+export default function FriendListItem({ friend }) {
+  const { avatar, name, isOnline } = friend;
 
-    return (
-        <li className={css.item}
-        >
-            <span className={css.status}>{isOnline}</span>
-            <img className={css.avatar}
-                 src={avatar}
-                 alt={name}
-                 width="48"
-            />
-            <p className={css.name}>{name}</p>
-        </li>
-    );
+  return (
+    <li className={css.item}>
+      <span className={css.status}>{isOnline ? 'Online' : 'Offline'}</span>
+      <img className={css.avatar}
+           src={avatar}
+           alt={name}
+           width='48'
+      />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
 }
